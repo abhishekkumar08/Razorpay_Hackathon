a7ccce45bdc39f8b6bf9bec1fb395a3b7e0d039b
anushruti11 <anushruti1132001@gmail.com>
2021-12-03 19:41:07 +0530

    added bihar destination badge

--- client/src/components/Homepage/TopDestination.jsx
+++ client/src/components/Homepage/TopDestination.jsx
@@ -1,5 +1,62 @@
-const TopDestination = () => {
-  return <div>Top Destination</div>;
-};
+import './Homepage.css';
+import './TopDestination.css';
+
+const TopDestination = ({ item }) => {
+  console.log(item.places);
+  return (
+    <div
+      style={{
+        borderRadius: '50px',
+        boxShadow: '30px 30px 60px #080c14,-30px -30px 60px #162034',
+        maxWidth: '1200px',
+        margin: '2rem',
+      }}
+    >
+      <div
+        style={{
+          padding: '1rem',
+        }}
+      >
+        <div
+          style={{
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <div style={{ width: '60rem', display: 'flex' }}>
+            <div>
+              <img src={item.mainImg} alt="Bihar" width="300" height="300" />
+            </div>
+            <div style={{ padding: '0rem 1rem 1rem 1rem' }}>
+              <div
+                style={{
+                  marginBottom: '0.5rem',
+                  fontSize: '3rem',
+                  fontWeight: '500',
+                }}
+              >
+                {item.name}
+              </div>
+              <div>{item.description}</div>
+              {/* badges */}
+              <div
+                style={{
+                  marginTop: '1rem',
+                  display: 'flex',
+                  flexWrap: 'wrap',
+                  justifyContent: 'space-evenly',
+                }}
+              >
+                {item.badges.map((badge) => {
+                  return <div className="destination-badge">{badge}</div>;
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+  )};
 
 export default TopDestination;
