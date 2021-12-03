5ac2ff4911989922e36978c739afef26d6227825
Abhishek Kumar <abhishek022kk@gmail.com>
2021-12-03 15:56:03 +0530

    deleted useless files and added the basic structure of the app

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    return ( <div>Hello header</div> );
+}
+ 
+export default Header;
---
+++ client/src/components/Homepage/Homepage.jsx
@@ -0,0 +1,5 @@
+const Homepage = () => {
+    return ( <div>Hello Homepage</div> );
+}
+ 
+export default Homepage;
