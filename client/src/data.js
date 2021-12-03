3bd0c5a02f3c880c46d8b60b35df466928672898
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-12-03 20:54:06 +0530

    added data for Delhi

--- client/src/data.js
+++ client/src/data.js
@@ -71,6 +71,150 @@ const data = [
       },
     ],
   },
+  {
+    id: '1',
+    name: 'Bihar',
+    description:
+      'Bihar is a place full of history and spiritual significance. Located in the eastern part of India, this state is known as the land of monasteries. There are many things that make this place worth visiting.',
+    badges: [
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+    ],
+    mainImg:
+      'https://images.unsplash.com/photo-1622194993926-1801586d460f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    places: [
+      {
+        area: 'Golghar',
+        description: 'This is golghar.',
+        subImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/patna/golghar/3300X2400.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        area: 'Mahabodhi Temple',
+        description: 'This is Mahabodhi Temple.',
+        subImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/gaya/mahabodhi_temple/buddhist_gaya_category_a_mahabodhi_temple__pic_13.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        area: 'Maner Sharif',
+        description: 'This is Maner Sharif.',
+        subImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/patna/maner_sharief/maner-sharif.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        area: 'Bihar Museum',
+        description: 'This is Bihar Museum.',
+        subImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/patna/bihar_museum/bihar-museum.png/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        area: 'Takhat Sri Harimandir Ji',
+        description: 'This is Patna Sahib.',
+        subImg: 'http://www.pmc.bihar.gov.in/assets/img/patna%20sahib.jpg',
+      },
+      {
+        area: 'Vishwa Shanti Stupa',
+        description: 'This is Vishwa Shanti Stupa.',
+        subImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/nalanda/shanti_stupa/vishwa-shanti-stupa-rajgir.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+    ],
+    items: [
+      {
+        itemName: 'Madhubani Painting',
+        description: 'This is Madhubani Painting.',
+        itemImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/stone/10.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        itemName: 'Stone Craft',
+        description: 'This is Stone Craft.',
+        itemImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/madhubani/madhubani_1.png/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        itemName: 'Wood Carvings',
+        description: 'This is Wood craft.',
+        itemImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/wood/4.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+    ],
+  },
+  {
+    id: '2',
+    name: 'Delhi',
+    description:
+      '"The Capital City of India", Delhi is a cosmopolitan city with a historic old Delhi and the modern New Delhi. You can\'t define Delhi in just few words. The city is too dynamic for that. It\'s rich history saturated with old stories reflects in beautiful heritage buildings tucked around the sprawling city. The dusty patina of the old stonewalls gives a rumbling beauty to the city landscape and one can almost hear the sounds of the past echoing.',
+    badges: [
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+    ],
+    mainImg:
+      'https://images.unsplash.com/photo-1622194993926-1801586d460f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    places: [
+      {
+        area: 'Red Fort',
+        description: 'This is Red Fort.',
+        subImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/Delhi_Red_fort_20190312151147.jpg',
+      },
+      {
+        area: 'Humanyun\'s Tomb',
+        description: 'This is Humanyun\'s Tomb.',
+        subImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/Humayun-tomb_20170809201316.jpg',
+      },
+      {
+        area: 'Qutab Minar',
+        description: 'This is Qutab Minar.',
+        subImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/Qutub_Minar_in_the_monsoons_20170908115259.jpg',
+      },
+      {
+        area: 'Lotus Temple',
+        description: 'This is Lotus Temple.',
+        subImg:
+          'https://holidify.com/images/attr_wiki/compressed/attr_wiki_57.jpg',
+      },
+      {
+        area: 'Akshardham Temple',
+        description: 'This is Akshardham Temple.',
+        subImg: 'https://www.holidify.com/images/cmsuploads/compressed/attr_wiki_4067_20191212124026.jpg',
+      },
+      {
+        area: 'India Gate',
+        description: 'This is India Gate.',
+        subImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/5621259188_e74d63cb05_b_20180302140149.jpg',
+      },
+    ],
+    items: [
+      {
+        itemName: 'Madhubani Painting',
+        description: 'This is Madhubani Painting.',
+        itemImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/stone/10.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        itemName: 'Stone Craft',
+        description: 'This is Stone Craft.',
+        itemImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/madhubani/madhubani_1.png/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+      {
+        itemName: 'Wood Carvings',
+        description: 'This is Wood craft.',
+        itemImg:
+          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/wood/4.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
+      },
+    ],
+  },
 ];
 
 export default data;
