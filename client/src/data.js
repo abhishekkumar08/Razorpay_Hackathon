de8f1fd74ec42d2b6c6dc84c0f88166f5c5ce26d
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2021-12-03 22:34:27 +0530

    added data for Delhi

--- client/src/data.js
+++ client/src/data.js
@@ -72,6 +72,78 @@ const data = [
     ],
   },
   {
+    id: '2',
+    name: 'Delhi',
+    description:
+      '"The Capital City of India", Delhi is a cosmopolitan city with a historic old Delhi and the modern New Delhi. You can\'t define Delhi in just few words. The city is too dynamic for that. It\'s rich history saturated with old stories reflects in beautiful heritage buildings tucked around the sprawling city. The dusty patina of the old stonewalls gives a rumbling beauty to the city landscape and one can almost hear the sounds of the past echoing.',
+    badges: [
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+      'Sightseeing Tours',
+    ],
+    mainImg:
+      'https://www.hotelierindia.com/public/styles/full_img/public/images/2020/03/26/travel-4813654_1920.jpg?itok=va5ff5Dk',
+    places: [
+      {
+        area: 'Red Fort',
+        description: 'This is Red Fort.',
+        subImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/Delhi_Red_fort_20190312151147.jpg',
+      },
+      {
+        area: 'Humanyun\'s Tomb',
+        description: 'This is Humanyun\'s Tomb.',
+        subImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/Humayun-tomb_20170809201316.jpg',
+      },
+      {
+        area: 'Qutab Minar',
+        description: 'This is Qutab Minar.',
+        subImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/Qutub_Minar_in_the_monsoons_20170908115259.jpg',
+      },
+      {
+        area: 'Lotus Temple',
+        description: 'This is Lotus Temple.',
+        subImg:
+          'https://holidify.com/images/attr_wiki/compressed/attr_wiki_57.jpg',
+      },
+      {
+        area: 'Akshardham Temple',
+        description: 'This is Akshardham Temple.',
+        subImg: 'https://www.holidify.com/images/cmsuploads/compressed/attr_wiki_4067_20191212124026.jpg',
+      },
+      {
+        area: 'India Gate',
+        description: 'This is India Gate.',
+        subImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/5621259188_e74d63cb05_b_20180302140149.jpg',
+      },
+    ],
+    items: [
+      {
+        itemName: 'Pottery',
+        description: 'Dilli Haat is the home of handicrafts.',
+        itemImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/Dilli_Haat_20190826181340.jpg',
+      },
+      {
+        itemName: 'Apparels, Accessories',
+        description: 'Sarojini Market, a shopping heaven in Delhi.',
+        itemImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/230671367_de70f22ad6_b_20190826181659.jpg',
+      },
+      {
+        itemName: 'Traditional and Ethnic apparels',
+        description: 'The vibrant Karol Bagh',
+        itemImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/Karol_Bagh2C_2008_281729_20190826182403.JPG',
+      },
+    ],
+  },
+  {
     id: '1',
     name: 'Bihar',
     description:
@@ -120,78 +192,6 @@ const data = [
         description: 'This is Vishwa Shanti Stupa.',
         subImg:
           'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/nalanda/shanti_stupa/vishwa-shanti-stupa-rajgir.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
-      },
-    ],
-    items: [
-      {
-        itemName: 'Madhubani Painting',
-        description: 'This is Madhubani Painting.',
-        itemImg:
-          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/stone/10.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
-      },
-      {
-        itemName: 'Stone Craft',
-        description: 'This is Stone Craft.',
-        itemImg:
-          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/madhubani/madhubani_1.png/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
-      },
-      {
-        itemName: 'Wood Carvings',
-        description: 'This is Wood craft.',
-        itemImg:
-          'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/wood/4.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
-      },
-    ],
-  },
-  {
-    id: '2',
-    name: 'Delhi',
-    description:
-      '"The Capital City of India", Delhi is a cosmopolitan city with a historic old Delhi and the modern New Delhi. You can\'t define Delhi in just few words. The city is too dynamic for that. It\'s rich history saturated with old stories reflects in beautiful heritage buildings tucked around the sprawling city. The dusty patina of the old stonewalls gives a rumbling beauty to the city landscape and one can almost hear the sounds of the past echoing.',
-    badges: [
-      'Sightseeing Tours',
-      'Sightseeing Tours',
-      'Sightseeing Tours',
-      'Sightseeing Tours',
-      'Sightseeing Tours',
-    ],
-    mainImg:
-      'https://images.unsplash.com/photo-1622194993926-1801586d460f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-    places: [
-      {
-        area: 'Red Fort',
-        description: 'This is Red Fort.',
-        subImg:
-          'https://www.holidify.com/images/cmsuploads/compressed/Delhi_Red_fort_20190312151147.jpg',
-      },
-      {
-        area: 'Humanyun\'s Tomb',
-        description: 'This is Humanyun\'s Tomb.',
-        subImg:
-          'https://www.holidify.com/images/cmsuploads/compressed/Humayun-tomb_20170809201316.jpg',
-      },
-      {
-        area: 'Qutab Minar',
-        description: 'This is Qutab Minar.',
-        subImg:
-          'https://www.holidify.com/images/cmsuploads/compressed/Qutub_Minar_in_the_monsoons_20170908115259.jpg',
-      },
-      {
-        area: 'Lotus Temple',
-        description: 'This is Lotus Temple.',
-        subImg:
-          'https://holidify.com/images/attr_wiki/compressed/attr_wiki_57.jpg',
-      },
-      {
-        area: 'Akshardham Temple',
-        description: 'This is Akshardham Temple.',
-        subImg: 'https://www.holidify.com/images/cmsuploads/compressed/attr_wiki_4067_20191212124026.jpg',
-      },
-      {
-        area: 'India Gate',
-        description: 'This is India Gate.',
-        subImg:
-          'https://www.holidify.com/images/cmsuploads/compressed/5621259188_e74d63cb05_b_20180302140149.jpg',
       },
     ],
     items: [
