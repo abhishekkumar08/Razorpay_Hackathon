3a4e6343746bd6a58992d356550315070c5380b3
Abhishek Kumar <abhishek022kk@gmail.com>
2021-12-04 21:51:42 +0530

    some ui changes

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import data from '../../data';
 import TopDestination from './TopDestination';
 import './Homepage.css';
+import Features from './Features';
 
 const Homepage = () => {
   const [state, setState] = useState('');
@@ -88,7 +89,6 @@ const Homepage = () => {
                 textAlign: 'center',
                 borderRadius: '50%',
                 margin: '1rem 3rem 1rem 1rem',
-                cursor: 'pointer',
               }}
             >
               <div>
@@ -107,80 +107,7 @@ const Homepage = () => {
       </div>
 
       {/* features */}
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-evenly',
-          padding: '2rem',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '22%',
-            textAlign: 'center',
-          }}
-        >
-          <div style={{ marginBottom: '0.5rem' }}>
-            <img
-              src="https://images.contentstack.io/v3/assets/blt06f605a34f1194ff/blt234df6e6b520e8c8/5eec6fff7253820a4364e1fe/ic-thumbs-up.svg"
-              alt="flexible booking"
-              width="30"
-              height="30"
-            />
-          </div>
-          <div style={{ marginBottom: '0.5rem' }}>Flexible Booking</div>
-          <div style={{ color: '#63687A' }}>
-            With free cancellation up to 24 hours before your activity, there’s
-            always room for a change of plans.
-          </div>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '22%',
-            textAlign: 'center',
-          }}
-        >
-          <div style={{ marginBottom: '0.5rem' }}>
-            <img
-              src="https://images.contentstack.io/v3/assets/blt06f605a34f1194ff/blt56797084b4e2d295/5e6bb7d58c83c26c32eb0bfa/icon-duration.svg"
-              alt="flexible booking"
-              width="30"
-              height="30"
-            />
-          </div>
-          <div style={{ marginBottom: '0.5rem' }}>Quality experiences</div>
-          <div style={{ color: '#63687A' }}>
-            Read millions of unfiltered reviews from GetYourGuide travelers to
-            help you find the best activities.
-          </div>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '22%',
-            textAlign: 'center',
-          }}
-        >
-          <div style={{ marginBottom: '0.5rem' }}>
-            <img
-              src="https://images.contentstack.io/v3/assets/blt06f605a34f1194ff/blt054c706d0acb19ee/5e6bb7e5b163ab6cedfa1c6d/icon-heart.svg"
-              alt="flexible booking"
-              width="30"
-              height="30"
-            />
-          </div>
-          <div style={{ marginBottom: '0.5rem' }}>Convenient support</div>
-          <div style={{ color: '#63687A' }}>
-            We’re here 24/7 to make your experience unforgettable, whether it’s
-            by phone, email, or WhatsApp.
-          </div>
-        </div>
-      </div>
+      <Features />
 
       {/* top destinations */}
       <div
@@ -194,7 +121,6 @@ const Homepage = () => {
         Explore our top destinations
       </div>
 
-        
       <div
         style={{
           display: 'flex',
