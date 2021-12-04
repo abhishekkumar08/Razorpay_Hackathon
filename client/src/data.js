4f58ac85f2f80013abb4a9fdb72295605b562f2d
Abhishek Kumar <abhishek022kk@gmail.com>
2021-12-04 23:40:21 +0530

    modified data.js

--- client/src/data.js
+++ client/src/data.js
@@ -18,36 +18,43 @@ const data = [
     places: [
       {
         area: 'Golghar',
-        description: 'An enormous granary built in 1786, after the terrible impact of 1770 famine.',
+        description:
+          'An enormous granary built in 1786, after the terrible impact of 1770 famine.',
         subImg:
           'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/patna/golghar/3300X2400.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
       },
       {
         area: 'Mahabodhi Temple',
-        description: 'An ancient Buddhist temple in Bodh Gaya, marking the location where the Buddha is said to have attained enlightenment.',
+        description:
+          'An ancient Buddhist temple in Bodh Gaya, marking the location where the Buddha is said to have attained enlightenment.',
         subImg:
           'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/gaya/mahabodhi_temple/buddhist_gaya_category_a_mahabodhi_temple__pic_13.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
       },
       {
         area: 'Maner Sharif',
-        description: 'Located 25 kilometers west of Patna, Maner Sharif used to be the principal site of learning and knowledge.',
+        description:
+          'Located 25 kilometers west of Patna, Maner Sharif used to be the principal site of learning and knowledge.',
         subImg:
           'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/patna/maner_sharief/maner-sharif.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
       },
       {
         area: 'Bihar Museum',
-        description: 'Located in Patna, Bihar Museum has a huge collection of artifacts that showcase the rich history and culture of Bihar.',
+        description:
+          'Located in Patna, Bihar Museum has a huge collection of artifacts that showcase the rich history and culture of Bihar.',
         subImg:
           'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/patna/bihar_museum/bihar-museum.png/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
       },
       {
         area: 'Patna Sahib',
-        description: 'Takhat Sri Harimandr Ji Patna Sahib is the second holiest Takhat. Revered as the birthplace of Sri Guru Gobind Singh Ji Maharaj.',
-        subImg: 'http://www.pmc.bihar.gov.in/assets/img/patna%20sahib.jpg',
+        description:
+          'Takhat Sri Harimandr Ji Patna Sahib is the second holiest Takhat. Revered as the birthplace of Sri Guru Gobind Singh Ji Maharaj.',
+        subImg:
+          'https://upload.wikimedia.org/wikipedia/commons/6/66/Harmandir_Patna.jpg',
       },
       {
         area: 'Vishwa Shanti Stupa',
-        description: 'Also known as Peace Pagoda, it is located at the highest point of Ratnagiri hill, at an altitude of 400 m in Rajgir.',
+        description:
+          'Also known as Peace Pagoda, it is located at the highest point of Ratnagiri hill, at an altitude of 400 m in Rajgir.',
         subImg:
           'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/category_a/nalanda/shanti_stupa/vishwa-shanti-stupa-rajgir.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
       },
@@ -55,19 +62,22 @@ const data = [
     items: [
       {
         itemName: 'Madhubani Painting',
-        description: 'Madhubani Painting has become synonymous with art and craft of Bihar in the whole world.',
+        description:
+          'Madhubani Painting has become synonymous with art and craft of Bihar in the whole world.',
         itemImg:
           'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/madhubani/madhubani_1.png/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
       },
       {
         itemName: 'Stone Craft',
-        description: 'A unique ancient handcraft of excellent artistic quality made out of marble and granite.',
+        description:
+          'A unique ancient handcraft of excellent artistic quality made out of marble and granite.',
         itemImg:
           'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/stone/10.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
       },
       {
         itemName: 'Wood Carvings',
-        description: 'Made out of Gambhar wood, having huge religious importance for the Buddhist followers and pilgrims.',
+        description:
+          'Made out of Gambhar wood, having huge religious importance for the Buddhist followers and pilgrims.',
         itemImg:
           'https://tourism.bihar.gov.in/content/dam/bihar-tourism/images/handicrafts_product_photo/wood/4.jpg/jcr:content/renditions/cq5dam.web.1280.765.jpeg',
       },
@@ -81,47 +91,54 @@ const data = [
     description:
       '"The Capital City of India", Delhi is a cosmopolitan city with a historic old Delhi and the modern New Delhi. You can\'t define Delhi in just few words. The city is too dynamic for that. It\'s rich history saturated with old stories reflects in beautiful heritage buildings tucked around the sprawling city.',
     badges: [
-        'Sightseeing Tours',
-        'Art & Crafts',
-        'Culture',
-        'Religious Places',
-        'Music & Dance',
+      'Sightseeing Tours',
+      'Art & Crafts',
+      'Culture',
+      'Religious Places',
+      'Music & Dance',
     ],
     mainImg:
       'https://www.hotelierindia.com/public/styles/full_img/public/images/2020/03/26/travel-4813654_1920.jpg?itok=va5ff5Dk',
     places: [
       {
         area: 'Red Fort',
-        description: 'A historical fortification in the old Delhi area. Every year, the Indian Prime Minister unfurls the national flag here on the Independence Day.',
+        description:
+          'A historical fortification in the old Delhi area. Every year, the Indian Prime Minister unfurls the national flag here on the Independence Day.',
         subImg:
           'https://www.holidify.com/images/cmsuploads/compressed/Delhi_Red_fort_20190312151147.jpg',
       },
       {
-        area: 'Humanyun\'s Tomb',
-        description: 'The final resting place of the Mughal Emperor Humayun. Located in Delhi, it is the first garden-tomb in the Indian subcontinent.',
+        area: "Humanyun's Tomb",
+        description:
+          'The final resting place of the Mughal Emperor Humayun. Located in Delhi, it is the first garden-tomb in the Indian subcontinent.',
         subImg:
           'https://www.holidify.com/images/cmsuploads/compressed/Humayun-tomb_20170809201316.jpg',
       },
       {
         area: 'Qutab Minar',
-        description: 'A minearet or a victory tower, Qutub Minar, a UNESCO World Heritage Site, is the second tallest monument in Delhi.',
+        description:
+          'A minearet or a victory tower, Qutub Minar, a UNESCO World Heritage Site, is the second tallest monument in Delhi.',
         subImg:
           'https://www.holidify.com/images/cmsuploads/compressed/Qutub_Minar_in_the_monsoons_20170908115259.jpg',
       },
       {
         area: 'Lotus Temple',
-        description: 'An edifice dedicated to the Baha\'i faith. The magnificent structure of this building unfolds in the form of a stupendous white petal lotus.',
+        description:
+          "An edifice dedicated to the Baha'i faith. The magnificent structure of this building unfolds in the form of a stupendous white petal lotus.",
         subImg:
           'https://holidify.com/images/attr_wiki/compressed/attr_wiki_57.jpg',
       },
       {
         area: 'Akshardham Temple',
-        description: 'Dedicated to Lord Swaminarayan. Akshardham has made its way to the Guinness Book of World Records as the World\'s Largest Comprehensive Hindu Temple.',
-        subImg: 'https://www.holidify.com/images/cmsuploads/compressed/attr_wiki_4067_20191212124026.jpg',
+        description:
+          "Dedicated to Lord Swaminarayan. Akshardham has made its way to the Guinness Book of World Records as the World's Largest Comprehensive Hindu Temple.",
+        subImg:
+          'https://www.holidify.com/images/cmsuploads/compressed/attr_wiki_4067_20191212124026.jpg',
       },
       {
         area: 'India Gate',
-        description: 'Dedicated to 82,000 Indian and British soldiers who died during the First World War and the Third Anglo-Afghan War, this monument has the names of 13,300 servicemen inscribed on its surface.',
+        description:
+          'Dedicated to 82,000 Indian and British soldiers who died during the First World War and the Third Anglo-Afghan War, this monument has the names of 13,300 servicemen inscribed on its surface.',
         subImg:
           'https://www.holidify.com/images/cmsuploads/compressed/5621259188_e74d63cb05_b_20180302140149.jpg',
       },
@@ -153,63 +170,65 @@ const data = [
     iconImg:
       'https://upload.wikimedia.org/wikipedia/commons/2/26/Seal_of_Maharashtra.png',
     description:
-      'Maharastra, often called \'Gateway to the Heart of India\'. Owing to its proximity to the Western Ghats, it is blessed with a picturesque backdrop of the montains on one side and has the beautiful Konkan Coastal belt on the other.',
+      "Maharastra, often called 'Gateway to the Heart of India'. Owing to its proximity to the Western Ghats, it is blessed with a picturesque backdrop of the montains on one side and has the beautiful Konkan Coastal belt on the other.",
     badges: [
-        'Sightseeing Tours',
-        'Art & Crafts',
-        'Culture',
-        'Religious Places',
-        'Music & Dance',
+      'Sightseeing Tours',
+      'Art & Crafts',
+      'Culture',
+      'Religious Places',
+      'Music & Dance',
     ],
-    mainImg:
-      'https://www.holidify.com/images/bgImages/MAHARASHTRA-GOA.jpg',
+    mainImg: 'https://www.holidify.com/images/bgImages/MAHARASHTRA-GOA.jpg',
     places: [
       {
         area: 'Mumbai',
-        description: 'The Financial Capital of India, City of Dreams - formerly known as Bombay.',
-        subImg:
-          'https://www.holidify.com/images/bgImages/MUMBAI.jpg',
+        description:
+          'The Financial Capital of India, City of Dreams - formerly known as Bombay.',
+        subImg: 'https://www.holidify.com/images/bgImages/MUMBAI.jpg',
       },
       {
         area: 'Shirdi',
         description: 'The home of Sai Baba.',
-        subImg:
-          'https://www.holidify.com/images/bgImages/SHIRDI.jpg',
+        subImg: 'https://www.holidify.com/images/bgImages/SHIRDI.jpg',
       },
       {
         area: 'Ajanta and Ellora Caves',
-        description: 'Considered as the finest example of ancient rock-cut caves, designated as UNESCO World Heritage Site.',
+        description:
+          'Considered as the finest example of ancient rock-cut caves, designated as UNESCO World Heritage Site.',
         subImg:
           'https://www.holidify.com/images/bgImages/AJANTA-AND-ELLORA-CAVES.jpg',
       },
       {
         area: 'Mahabaleshwar',
-        description: 'A hill station located in the Western Ghats, well known for its strawberries, numerous rivers and majestic peaks.',
-        subImg:
-          'https://www.holidify.com/images/bgImages/MAHABALESHWAR.jpg',
+        description:
+          'A hill station located in the Western Ghats, well known for its strawberries, numerous rivers and majestic peaks.',
+        subImg: 'https://www.holidify.com/images/bgImages/MAHABALESHWAR.jpg',
       },
       {
         area: 'Nashik',
-        description: 'The religious Hindu city, host to the Kumbh Mela every 12 years.',
+        description:
+          'The religious Hindu city, host to the Kumbh Mela every 12 years.',
         subImg: 'https://www.holidify.com/images/bgImages/NASIK.jpg',
       },
       {
         area: 'Aurangabad',
-        description: 'The Tourism Capital of Maharastra, home to Ajanta and Ellora Caves.',
-        subImg:
-          'https://www.holidify.com/images/bgImages/AURANGABAD.jpg',
+        description:
+          'The Tourism Capital of Maharastra, home to Ajanta and Ellora Caves.',
+        subImg: 'https://www.holidify.com/images/bgImages/AURANGABAD.jpg',
       },
     ],
     items: [
       {
         itemName: 'Kolhauri Chappals',
-        description: 'World famous hand-made leather chappals and sandals of Kolhapur.',
+        description:
+          'World famous hand-made leather chappals and sandals of Kolhapur.',
         itemImg:
           'https://www.outlookindia.com/traveller/wp-content/uploads/2017/03/ART-AND-CRAFTS3_TI.jpg',
       },
       {
         itemName: 'Paithani Sari',
-        description: 'Sarees made of finest silk with an ornamented zari pallav and border.',
+        description:
+          'Sarees made of finest silk with an ornamented zari pallav and border.',
         itemImg:
           'https://www.outlookindia.com/traveller/wp-content/uploads/2017/03/ART-AND-CRAFTS2_TI.jpg',
       },
