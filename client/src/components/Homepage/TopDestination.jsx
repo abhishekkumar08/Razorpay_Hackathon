555ea2b8a46db4fafd34cb67a7c39d6fa92a51b1
Abhishek Kumar <abhishek022kk@gmail.com>
2021-12-04 20:15:18 +0530

    added backend of the app and integrated it with the frontend

--- client/src/components/Homepage/TopDestination.jsx
+++ client/src/components/Homepage/TopDestination.jsx
@@ -1,7 +1,59 @@
 import './Homepage.css';
 import './TopDestination.css';
 
+function loadScript(src) {
+  return new Promise((resolve) => {
+    const script = document.createElement('script');
+    script.src = src;
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
+}
+
 const TopDestination = ({ item }) => {
+  async function displayRazorpay() {
+    const res = await loadScript(
+      'https://checkout.razorpay.com/v1/checkout.js'
+    );
+
+    if (!res) {
+      alert('Razorpay SDK failed to load. Are you online?');
+      return;
+    }
+
+    const data = await fetch('http://localhost:1337/razorpay', {
+      method: 'POST',
+    }).then((t) => t.json());
+
+    console.log('dataaa', data);
+
+    const options = {
+      key: 'rzp_test_7NFjEzrn3WQR0c',
+      currency: data.currency,
+      amount: data.amount.toString(),
+      order_id: data.id,
+      name: 'Donation',
+      description: 'Thank you for nothing. Please give us some money',
+      image: 'http://localhost:1337/logo.svg',
+      handler: function (response) {
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature);
+      },
+      prefill: {
+        name: '',
+        email: '',
+        phone_number: '',
+      },
+    };
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  }
   console.log(item.places);
   return (
     <div
@@ -121,7 +173,11 @@ const TopDestination = ({ item }) => {
                 </div>
 
                 <div>
-                  <button type="submit" className="card-stats">
+                  <button
+                    type="submit"
+                    className="card-stats"
+                    onClick={displayRazorpay}
+                    >
                     Order Now!
                   </button>
                 </div>
@@ -181,7 +237,11 @@ const TopDestination = ({ item }) => {
                   </div>
 
                   <div>
-                    <button type="submit" className="card-stats">
+                    <button
+                      type="submit"
+                      className="card-stats"
+                      onClick={displayRazorpay}
+                    >
                       Order Now!
                     </button>
                   </div>
